c2d19110b0976eab6abbed1ed9235d65100dfa0a
Daniel Bogucki <daniel.bogucki14@gmail.com>
2022-02-09 18:44:09 +0100

    Worked on operateInput fc - can now switch between operators

--- scripts.js
+++ scripts.js
@@ -3,11 +3,12 @@ const numbers = document.querySelectorAll('.number');
 const equal = document.querySelector('.equal');
 const clear = document.querySelector('.clear');
 const screen = document.querySelector('.screen');
+
 let input = "";
-let count;
 let resultsArray = [];
 let lastOperator;
 let lastButton;
+let lastInput;
 let resultDisplayed = false;
 
 function enterInput(e) {
@@ -24,17 +25,29 @@ function enterInput(e) {
 
 function operateInput(e) {
     resultsArray.push(parseFloat(input));
+    
     if (!resultDisplayed) {
         this.classList.add('hl');
         resultDisplayed = true;
+    } else {
+        lastButton.classList.remove('hl');
+        this.classList.add('hl');
+    }
+
+    if (resultsArray.length == 2) {
+        resultEqual();
+        resultsArray.push(parseFloat(input));
     }
 
     lastOperator = e.target.dataset.key;
     lastButton = this;
+    lastInput = parseFloat(input);
 }
 
 function resultEqual() {
+   if (!lastOperator) return;
     resultsArray.push(parseFloat(input));
+    console.log(input, resultsArray);
     
     switch (lastOperator) {
         case ('+'):
@@ -51,13 +64,20 @@ function resultEqual() {
             break;
     }
 
+    if (input === Infinity) {
+                input = "ERROR";
+            }
+
     screen.textContent = input;
-    resultsArray = [];
+    resultsArray = [input, lastInput];
+
 }
 
 function clearInput() {
     input = "";
     screen.textContent = "";
+    lastOperator = "";
+    if (lastButton) lastButton.classList.remove('hl');
 }
 
 clear.addEventListener('click', clearInput);
